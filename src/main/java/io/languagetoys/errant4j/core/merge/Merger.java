d11aa6271a7bed2e7c54f7e786e503d3df486e23
manzurola <guy.manzurola@gmail.com>
2021-07-08 22:05:02 +0300

    add some docs

--- src/main/java/io/languagetoys/errant4j/core/merge/Merger.java
+++ src/main/java/io/languagetoys/errant4j/core/merge/Merger.java
@@ -13,18 +13,30 @@ public interface Merger {
      */
     List<Edit<Token>> merge(List<Edit<Token>> edits);
 
+    /**
+     * Get a new merger that doesn't merge any edit.
+     */
     static Merger allSplit() {
         return new MergerImpl((a, b) -> false);
     }
 
+    /**
+     * Get a new merger that merges all edits.
+     */
     static Merger allMerge() {
         return new MergerImpl((a, b) -> true);
     }
 
+    /**
+     * Get a new merger that merges edits of equal operation.
+     */
     static Merger allEqual() {
         return new MergerImpl((a, b) -> a.operation().equals(b.operation()));
     }
 
+    /**
+     * Get a new rule based merger.
+     */
     static Merger rules(List<Rule> rules) {
         return new MergerImpl((a, b) -> rules.stream().anyMatch(rule -> rule.test(a, b)));
     }
