1f9d87388f981846098887aa6415383f306c0e9b
manzurola <guy.manzurola@gmail.com>
2021-07-08 22:06:56 +0300

    merge pipeline and basepipeline

--- src/main/java/io/languagetoys/errant4j/core/Pipeline.java
+++ src/main/java/io/languagetoys/errant4j/core/Pipeline.java
@@ -1,6 +1,7 @@
 package io.languagetoys.errant4j.core;
 
 import io.languagetoys.aligner.Aligner;
+import io.languagetoys.errant4j.core.align.TokenAligner;
 import io.languagetoys.errant4j.core.classify.Classifier;
 import io.languagetoys.errant4j.core.merge.Merger;
 import io.languagetoys.spacy4j.api.containers.Token;
@@ -12,4 +13,13 @@ public interface Pipeline {
     Merger getMerger();
 
     Classifier getClassifier();
+
+    abstract class Base implements Pipeline {
+
+        @Override
+        public Aligner<Token> getAligner() {
+            return new TokenAligner();
+        }
+
+    }
 }
